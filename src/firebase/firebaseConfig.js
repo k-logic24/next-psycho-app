ec87c4fd2823991f18663f96cbf04fd17d10c96f
岩ちゃん <iwater0715@gmail.com>
2020-05-16 00:00:45 +0900

    WIP

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: 'AIzaSyAWlHd7_M7zQEL0S9BXWcGWwSxiiOzf6QI',
+  authDomain: 'psychopath-diagnosis.firebaseapp.com',
+  databaseURL: 'https://psychopath-diagnosis.firebaseio.com',
+  projectId: 'psychopath-diagnosis',
+  storageBucket: 'psychopath-diagnosis.appspot.com',
+  messagingSenderId: '548786231928',
+  appId: '1:548786231928:web:b8e23fc11030ae7c682b8d'
+}
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,15 +1,52 @@
-import React from 'react'
-import {Form, Button} from 'react-bootstrap'
+import React, { useState } from 'react'
+import {
+  Form,
+  Button,
+} from 'react-bootstrap'
 
 import Layout from '@/layouts/default'
 
 const Home = () => {
+  const [title, setTitle] = useState('')
+  const [question, setQuestion] = useState('')
+  const [normal, setNormal] = useState('')
+  const [abnormal, setAbnormal] = useState('')
+
+  const handleChangeData = (value: string, target: string) => {
+    switch (target) {
+      case 'title':
+        setTitle(value)
+        break
+      case 'question':
+        setQuestion(value)
+        break
+      case 'normal':
+        setNormal(value)
+        break
+      case 'abnormal':
+        setAbnormal(value)
+        break
+    }
+  }
+
+  const handleClickUpload = () => {
+    const data = {
+      title: title,
+      question: question,
+      normal: normal,
+      abnormal: abnormal,
+    }
+
+    console.log(data)
+  }
+
   return (
     <Layout title="問題登録" description="各項目を記入して登録が可能です。">
       <Form.Group controlId="title">
         <Form.Label className="font-weight-bold">題名</Form.Label>
         <Form.Control
           type="text"
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleChangeData(e.target.value, 'title')}
         />
       </Form.Group>
       <Form.Group controlId="question">
@@ -17,6 +54,7 @@ const Home = () => {
         <Form.Control
           as="textarea"
           rows="5"
+          onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => handleChangeData(e.target.value, 'question')}
         />
       </Form.Group>
       <Form.Group controlId="normal">
@@ -24,6 +62,7 @@ const Home = () => {
         <Form.Control
           as="textarea"
           rows="5"
+          onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => handleChangeData(e.target.value, 'normal')}
         />
       </Form.Group>
       <Form.Group controlId="abnormal">
@@ -31,9 +70,15 @@ const Home = () => {
         <Form.Control
           as="textarea"
           rows="5"
+          onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => handleChangeData(e.target.value, 'abnormal')}
         />
       </Form.Group>
-      <Button variant="primary">登録する</Button>
+      <Button
+        variant="primary"
+        onClick={handleClickUpload}
+      >
+        登録する
+      </Button>
     </Layout>
   )
 }
